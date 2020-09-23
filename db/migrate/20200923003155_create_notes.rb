4a248628feadcc371aff42e6f839d5dc21f52731
Trevor <tlow94@uw.edu>
2020-09-22 17:33:19 -0700

    models

--- db/migrate/20200923003155_create_notes.rb
+++ db/migrate/20200923003155_create_notes.rb
@@ -1,6 +1,7 @@
 class CreateNotes < ActiveRecord::Migration[6.0]
   def change
     create_table :notes do |t|
+      t.string :text
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_23_001855) do
+ActiveRecord::Schema.define(version: 2020_09_23_003155) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "notes", force: :cascade do |t|
+    t.string "text"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
